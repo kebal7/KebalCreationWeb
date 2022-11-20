afe5fa717f0f11f81af3ada2172cee4daf5b4520
Kebal <kebal.badal555@gmail.com>
2022-11-20 18:02:16 +0545

    responsive navbar

--- javascript/nav.js
+++ javascript/nav.js
@@ -1,27 +1,11 @@
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
-    const navlinks = document.querySelectorAll('.nav-links li');
 
 
     burger.addEventListener('click',()=>{
-        //Toggle nav
         nav.classList.toggle('nav-active');
-
-        //Animate Links
-        navlinks.forEach((link, index)=>{
-            if(link.style.animation){
-            link.style.animation = ''
-            } else{
-            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 1}s`
-            }
-        });
-
-        //Burger Animation
-        burger.classList.toggle('toggle');
-
     });
-
 }
 
 navSlide();
